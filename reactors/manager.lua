1a3e6cca42f0ae924f5eadefa580ab847693cd00
jgchoppe <jean-gael.choppe@epitech.eu>
2023-03-19 02:04:12 +0100

    manager.lua

--- reactors/manager.lua
+++ reactors/manager.lua
@@ -4,6 +4,9 @@ local event = require('event')
 local Commands = require("enums.commands")
 local reactors = {}
 local managers = {}
+local component = require("component")
+local m = component.modem
+local computer = component.computer
 
 local name = args[1]
 if name == nil then
@@ -24,7 +27,10 @@ end
 
 local port = GetArgNumber(args[3], 80)
 
+
 print("Port: " .. port)
+
+m.open(port)
 
 if managerName ~= nil then
     isMain = false
@@ -40,8 +46,33 @@ end
 
 Start()
 
-function Register()
-    print("Register")
+function Register(data)
+    print(data.machineName)
+    print(data.machineAddress)
+    if data.managerName ~= name then
+        print("fail")
+        return
+    end
+
+    if data.type == "reactor" then
+        reactors[data.machineName] = {
+            name = data.machineName,
+            address = data.machineAddress
+        }
+        print("ok")
+    else if data.type == "manager" then
+            print("Not implemented.")
+        end
+    end
+
+    m.send(data.machineAddress, port, json.serialize({
+        command = Commands.RegisterCallback,
+        data = {
+            success = true,
+            managerAddress = computer.address
+        }
+    }))
+    print("send")
 end
 
 local handler = {
@@ -49,26 +80,28 @@ local handler = {
 }
 
 function HandleMsg(data)
+    print(data)
     local msg = json.unserialize(data)
+    print(msg.command)
     
     if handler[msg.command] ~= nil then
-        handler[msg.command]()
+        handler[msg.command](msg.data)
     end
 end
 
-local exampleData = {
-    command = "test"
-}
+-- local exampleData = {
+--     command = "test"
+-- }
 
-local dataToSend = json.serialize(exampleData)
+-- local dataToSend = json.serialize(exampleData)
 
-HandleMsg(dataToSend)
+-- HandleMsg(dataToSend)
 
 local globalCondition = true
 
--- while globalCondition do
---     local _, _, _, _, _, data = event.pull("modem_message")
---     if data ~= nil then
---         HandleMsg(data)
---     end
--- end
+while globalCondition do
+    local _, _, _, _, _, data = event.pull("modem_message")
+    if data ~= nil then
+        HandleMsg(data)
+    end
+end
