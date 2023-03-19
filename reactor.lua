057f22e48c7cfca0924e53bedb19abd78b0c58ca
Clement SCHMITTE <clement.schmitte@epitech.eu>
2023-03-19 01:10:52 +0100

    reactor with register and callback check

--- reactor.lua
+++ reactor.lua
@@ -1,14 +1,27 @@
 local gComponent = require('component')
 local gEvent = require('event')
-local gSerialization = require('serialization')
+local gComputer = require('computer')
+local gTerm = require("term")
+local gGpu = gComponent.gpu
 local gModem = gComponent.modem
+local json = require('serialization')
 local Commands = require('enums.commands')
 
 local globalArgv = { ... }
 local port = 80
+local timeoutTime = 10 -- in seconds
 
+-- Reactor
 local reactorName = globalArgv[1]
+
+-- Manager
 local managerName = globalArgv[2]
+local managerAdress = nil
+
+
+--------------------
+--      Init      --
+--------------------
 
 -- Print helper message
 function PrintHelp()
@@ -30,43 +43,99 @@ function ValidateArgs(args)
 end
 
 function Register()
-    local res = gModem.broadcast(port, reactorName)
+    local params = {
+        command = Commands.Register,
+        data = {
+            type = "reactor",
+            machineAddress = gComputer.address(),
+            machineName = reactorName,
+            managerName = managerName
+        }
+    }
+    local res = gModem.broadcast(port, json.serialize(params))
+    if (res == true) then
+        print("Trying to register...")
+    end
 end
 
+
+--------------------
+--      Utils     --
+--------------------
+
+
+function ErrorMessage(msg)
+    gGpu.setForeground(0xff0000)
+    print('ERROR: '.. msg)
+    gGpu.setForeground(0x000000)
+end
+
+
+
+--------------------
+-- Loop Functions --
+--------------------
+
 function ParseMsg(msgRaw)
-    local msgParsed = gSerialization.unserialize(msgRaw)
+    if (msgRaw == nil) then
+        return nil
+    end
+    local msgParsed = json.unserialize(msgRaw)
     local command = msgParsed.command
     local data = msgParsed.data
 
     return command, data
 end
 
--------------------
--- Main function --
--------------------
+function RegisterCallback(data)
+    if (data.success == true) then
+        print('Successfully registered to "' .. managerName .. '" on port: ' .. port .. '\nReactor name: ' .. reactorName)
+        if (data.managerAdress ~= nil) then
+            managerAdress = data.managerAdress
+        end
+        return true
+    else
+        ErrorMessage('Failed when registering to "' .. managerName .. '" on port "' .. port .. '"')
+    end
+    return false
+end
+
+
+--------------------
+--      Main      --
+--------------------
 
 local globalCondition = true
+local callbackRegister = false
 
 ValidateArgs(globalArgv)
 
+
+gTerm.clear()
+
+
 Register()
+
+function CheckRegisterCallback()
+    ErrorMessage('Failed when registering to "' .. managerName .. '" on port "' .. port .. '"')
+    if (callbackRegister == false) then
+        globalCondition = false
+    end
+end
 
 while globalCondition do
     -- Wait for an inbound message
-    local _, _, _, _, _, msgRaw = gEvent.pull("modem_message")
+    local _, _, _, _, _, msgRaw = gEvent.pull(timeoutTime, "modem_message")
 
     -- Parse message
     local command, data = ParseMsg(msgRaw)
 
     if (command == Commands.RegisterCallback) then
-        
+        callbackRegister = RegisterCallback(data)
     end
+
+    CheckRegisterCallback()
 end
 
+os.exit()
 
-
--- if (callbackRegister == true) then
---     print('Successfully registered to "' .. managerName .. '" on port: ' .. port .. '\nReactor name: ' .. reactorName)
--- else
---     print('Failed when registering to "' .. managerName .. '" on port "' .. port .. '"')
--- end
